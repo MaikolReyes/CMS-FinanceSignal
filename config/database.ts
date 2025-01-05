164f63a190f0253754ebcc3d2419d6f506135c7f
Maikol Reyes <maikolreyes209@gmail.com>
2025-01-05 17:38:37 -0300

    update

--- config/database.ts
+++ config/database.ts
@@ -24,19 +24,12 @@ export default ({ env }) => {
     },
     postgres: {
       connection: {
-        connectionString: env('DATABASE_URL'),
         host: env('DATABASE_HOST'),
-        port: env.int('DATABASE_PORT'),
+        port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME'),
         user: env('DATABASE_USER'),
         password: env('DATABASE_PASSWORD'),
-        ssl: env.bool('DATABASE_SSL', false) && {
-          key: env('DATABASE_SSL_KEY', undefined),
-          cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
-          capath: env('DATABASE_SSL_CAPATH', undefined),
-          cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
+        ssl: env.bool('DATABASE_SSL', true),
         },
         schema: env('DATABASE_SCHEMA', 'public'),
       },
