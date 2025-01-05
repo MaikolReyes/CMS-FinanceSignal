5bc922982eb9778cab97508241dc31ff03e5806b
Maikol Reyes <maikolreyes209@gmail.com>
2025-01-05 18:36:21 -0300

    admin secret

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,8 @@
-export default () => ({});
+export default {
+    'users-permissions': {
+        config: {
+            jwtSecret: process.env.ADMIN_JWT_SECRET || 'default-secret',  // Asegúrate de que lea desde el archivo .env
+        },
+    },
+};
+
