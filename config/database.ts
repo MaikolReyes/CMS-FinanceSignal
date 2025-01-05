92ca3eda1d29949a5c9bf385c8afc0e560c7fb41
Maikol Reyes <maikolreyes209@gmail.com>
2025-01-05 17:53:33 -0300

    update 4

--- config/database.ts
+++ config/database.ts
@@ -31,7 +31,6 @@ export default ({ env }) => {
         password: env('DATABASE_PASSWORD'),
         ssl: env.bool('DATABASE_SSL', true),
       },
-      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
     },
     sqlite: {
       connection: {
