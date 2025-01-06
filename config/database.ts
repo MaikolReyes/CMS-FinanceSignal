6a066b86f6df4d3a6969328b13ac7ab8f3c5cd18
Maikol Reyes <maikolreyes209@gmail.com>
2025-01-06 20:47:31 -0300

    correction in SSL

--- config/database.ts
+++ config/database.ts
@@ -25,11 +25,13 @@ export default ({ env }) => {
     postgres: {
       connection: {
         host: env('DATABASE_HOST'),
-        port: env.int('DATABASE_PORT',5432),
+        port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME'),
         user: env('DATABASE_USERNAME'),
         password: env('DATABASE_PASSWORD'),
-        ssl: env.bool('DATABASE_SSL', true),
+        ssl: {
+          rejectUnauthorized: false, // Permitir certificados autofirmados
+        },
       },
     },
     sqlite: {
