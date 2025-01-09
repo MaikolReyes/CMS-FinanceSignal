1d6cb1b32cf61f5da975a4c44ca9698816f697ac
Maikol Reyes <maikolreyes209@gmail.com>
2025-01-09 20:24:31 -0300

    middlewares update

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -3,6 +3,14 @@ export default [
   'strapi::errors',
   'strapi::security',
   'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:5173'], // Permitir localhost
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Métodos permitidos
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
