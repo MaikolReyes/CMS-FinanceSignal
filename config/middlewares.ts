23fc43d348afa502e020018aa45a04204d0e00fc
Maikol Reyes <maikolreyes209@gmail.com>
2025-02-19 12:18:36 -0300

    changes in middle

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -13,14 +13,14 @@ export default [
             'data:',
             'blob:',
             'dl.airtable.com',
-            `https://${process.env.AWS_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com`,
+            `${process.env.AWS_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com`,
           ],
           'media-src': [
             "'self'",
             'data:',
             'blob:',
             'dl.airtable.com',
-            `https://${process.env.AWS_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com`,
+            `${process.env.AWS_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com`,
           ],
           upgradeInsecureRequests: null,
         },
@@ -31,7 +31,7 @@ export default [
   {
     name: 'strapi::cors',
     config: {
-      origin: ['http://localhost:5173', 'https://cms-financesignal.onrender.com'], // Permitir localhost
+      origin: ['http://localhost:5173'], // Permitir localhost
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Métodos permitidos
       headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
     },
